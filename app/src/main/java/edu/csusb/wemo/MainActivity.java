857cea4b07999563115d2472f9bb9a23dfff1f83
Nicholas <dragonfirenick4@yahoo.com>
2017-02-15 14:13:53 -0800

    Added Message to main activity

--- app/src/main/java/edu/csusb/wemo/MainActivity.java
+++ app/src/main/java/edu/csusb/wemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //Nick Test Change
+    //Testing numbah two...
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
